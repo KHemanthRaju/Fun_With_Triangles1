fef4b240fb3dd811f09cf92d30da4d4bbaae42d2
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2022-10-28 23:43:38 +0530

    Made changes

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ var hypoOutput = document.getElementById("hyporesult")
 hypo.addEventListener("click",()=>{
     var inputA =a.value;
     var inputB = b.value;
-    var sol = Math.sqrt(((inputA*inputA)+(inputB*inputB))).toFixed(2);
-    hypoOutput.innerHTML = "c = " + sol;
+    if(inputA.length>0 && inputB.length>0){
+        var sol = Math.sqrt(((inputA*inputA)+(inputB*inputB))).toFixed(2);
+        hypoOutput.innerHTML = "c = " + sol;
+    }else{
+        hypoOutput.innerHTML = "Invalid inputs. Please fill all the fields";
+    }
+    
 })
 
